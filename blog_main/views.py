128197f6c4b378b52f404830ff5ed26da9b12ba3
Bakul <ce23014@mbstu.ac.bd>
2026-04-15 22:15:07 +0600

    add base and context_processors and modified some files

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 
 from blogs.models import Blog, Categories
 def home(request):
-    categories = Categories.objects.all()
     featured_posts = Blog.objects.filter(Is_featured=True,Status="Published").order_by('-Created_at')
     posts = Blog.objects.filter(Is_featured=False, Status="Published").order_by('-Created_at')
     context = {
-        'categories': categories,
         'featured_posts': featured_posts,
         'posts': posts,
     }
--- blogs/views.py
+++ blogs/views.py
@@ -1,23 +1,13 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Blog, Categories
 
 def post_by_category(request, Category_id):
-    # Logic to retrieve posts based on the Category_id
-    # For example, you might query the Blog model to get posts in that category
+    category = get_object_or_404(Categories, pk=Category_id)
     posts = Blog.objects.filter(Category_id=Category_id, Status="Published").order_by('-Created_at')
-    try:
-        # Retrieve the category name based on the Category_id
-        category = Categories.objects.get(pk=Category_id)
-    except:
-        # If the category does not exist, you can handle it as needed (e.g., show an error message or redirect)
-        return redirect('home')
 
-    # category = get_object_or_404(Categories, pk=Category_id)
-
-        
     context = {
         'posts': posts,
-        'category':category
+        'category': category,
     }
     return render(request, 'post_by_category.html', context)
